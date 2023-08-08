d20e4cac9bcbdd6f0fb8c88c1eefdd0ab5302208
sanjaahlam <ahlem_sanja@hotmail.com>
2023-08-08 21:23:16 +0200

    commit

---
+++ Expanding cards/index.js
@@ -0,0 +1,12 @@
+const panels = document.querySelectorAll(".panel");
+panels.forEach((panel) => {
+    panel.addEventListener("click", () => {
+        removeActiveClasses();
+        panel.classList.add("active");
+    });
+})
+const removeActiveClasses= function () {
+    panels.forEach((panel) => {
+        panel.classList.remove("active");
+    })
+}
